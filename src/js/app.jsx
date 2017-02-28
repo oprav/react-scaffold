5265ec667453c2789fefdbe9efa8d3b5441f0a08
Oleksandr Pravosudko <opravosudko@amplify.com>
2017-02-28 19:00:20 +0200

    adding layout & component placeholder files

---
+++ src/js/app.jsx
@@ -0,0 +1,9 @@
+'use strict';
+
+import React from 'react';
+import ReactDOM from 'react-dom';
+import Menu from './components/Menu.jsx'
+
+ReactDOM.render(
+    <Menu />, document.getElementById('app')
+);
